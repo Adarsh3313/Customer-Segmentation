80107d18025023adf0c2272bfce62da9b1bddaf7
Adarsh3313 <adarshreddymiddi@gmail.com>
2025-08-17 17:34:29 +0530

    Update update.py

--- uppdate.py
+++ uppdate.py
@@ -1,14 +1,4 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "id": "c949c7e1-66fa-4085-a363-1b6f40d3bc5d",
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  }
- ],
+
  "metadata": {
   "kernelspec": {
    "display_name": "",
