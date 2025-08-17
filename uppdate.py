4e8d3fb71cf39947c33cfd28d1e4d14c564f6a4d
Adarsh3313 <adarshreddymiddi@gmail.com>
2025-08-17 17:30:54 +0530

    Add files via upload

---
+++ uppdate.py
@@ -0,0 +1,23 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "c949c7e1-66fa-4085-a363-1b6f40d3bc5d",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "",
+   "name": ""
+  },
+  "language_info": {
+   "name": ""
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
